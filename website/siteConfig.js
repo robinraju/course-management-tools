7c2b6e8e069433385c122116345a1cd3b7462a47
Robin Raju <robinraju@users.noreply.github.com>
2020-07-31 18:09:42 +0200

    Update siteconfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -55,20 +55,6 @@ const siteConfig = {
     secondaryColor: '#00A900',
   },
 
-  /* Custom fonts for website */
-  /*
-  fonts: {
-    myFont: [
-      "Times New Roman",
-      "Serif"
-    ],
-    myOtherFont: [
-      "-apple-system",
-      "system-ui"
-    ]
-  },
-  */
-
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
   copyright: `Copyright © ${new Date().getFullYear()} Course Management Tools`,
 
